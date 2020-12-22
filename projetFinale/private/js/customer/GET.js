cd2ea3257698f5d928afce4ee7ba5759ee2445c9
klikman1 <mmihigojonathan@gmail.com>
2020-12-22 16:41:50 +0100

    Started doing tests

--- projetFinale/private/js/customer/GET.js
+++ projetFinale/private/js/customer/GET.js
@@ -63,6 +63,7 @@ function signUp(app,req,res){
 }
 function restaurantsPage(app,req,res){
     restaurantModel.findOne({ name : req.query.name }).then((rest) => {
+        
         restaurantModel.findById(rest._id).then(async (restaurant) => {
             const restaurantObject = await restaurant.getRestaurantView();
             res.render('./customer/RestaurantViewPage.html', {
