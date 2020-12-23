0dabe22346d09a9742f79dc36cc42bbaef4a6554
hirwaraissa <56304641+hirwaraissa@users.noreply.github.com>
2020-12-23 12:02:00 +0100

    Comments done

--- projetFinale/private/js/seller/DELETE.js
+++ projetFinale/private/js/seller/DELETE.js
@@ -18,6 +18,13 @@ function deleteAnItemFromARestaurant(app, req, res){
     })
 }
 
+/**
+ * This function deletes a group in a restaurant
+ * @param app
+ * @param req
+ * @param res
+ */
+
 function deleteAGroupFromARestaurant(app, req, res){
     restaurantModel.findById(req.session.restaurant._id).then((restaurant) => {
         restaurant.removeGroup(req.body.name)
@@ -30,6 +37,12 @@ function deleteAGroupFromARestaurant(app, req, res){
     })
 }
 
+/**This function deletes a category in a restaurant
+ *
+ * @param app
+ * @param req
+ * @param res
+ */
 function deleteACategoryFromRestaurant(app, req, res){
     restaurantModel.findById(req.session.restaurant._id).then((restaurant) => {
         restaurant.removeCategory(req.body.name)
--- projetFinale/private/js/seller/GET.js
+++ projetFinale/private/js/seller/GET.js
@@ -2,6 +2,12 @@ const {setVirtualImageSrc} = require("../general/functions");
 const { restaurantModel } = require('./../general/schemas');
 const { formatText } = require('./../general/functions')
 
+/**
+ * This function adds or modifies a group in a given restaurant
+ * @param app
+ * @param req
+ * @param res
+ */
 function addOrModifyGroup(app, req, res){
     restaurantModel.findById(req.session.restaurant._id)
         .then(async (restaurant) => {
@@ -23,6 +29,13 @@ function addOrModifyGroup(app, req, res){
             }
         })
     }
+
+/**
+ * This function adds or modifies an item in a given restaurant
+  * @param app
+ * @param req
+ * @param res
+ */
 
 function addOrModifyItem(app, req, res){
     restaurantModel.findById(req.session.restaurant._id).then(async (restaurant) => {
@@ -50,6 +63,12 @@ function addOrModifyItem(app, req, res){
     })
 }
 
+/**
+ * This function adds or modifies a category in a given restaurant
+ * @param app
+ * @param req
+ * @param res
+ */
 function addOrModifyCategory(app, req, res){
     restaurantModel.findById(req.session.restaurant._id).then(async (restaurant) => {
         if (req.query.category){
@@ -63,14 +82,32 @@ function addOrModifyCategory(app, req, res){
     })
 }
 
+/**
+ * This function renders the Final Message when a restaurant is created
+ * @param app
+ * @param req
+ * @param res
+ */
 function createRestoFinishedMessage(app, req, res){
     res.render('./seller/CreateRestaurantFinished.html')
 }
 
+/**
+ * This function renders the page to create a restaurant
+ * @param app
+ * @param req
+ * @param res
+ */
 function createResto(app,req, res){
     res.render('./seller/CreateRestaurantSpeci.html');
 }
 
+/**
+ * This function renders the restaurant's dashboard with all the orders(if it exists)
+ * @param app
+ * @param req
+ * @param res
+ */
 function dashboard(app, req, res){
     restaurantModel.findById(req.session.restaurant._id).then(async (restaurant) => {
         const date = (req.query.date != null) ? new Date(req.query.date) : new Date();
@@ -90,6 +127,13 @@ function dashboard(app, req, res){
     })
 }
 
+/**
+ * This function returns the details about an order in a restaurant
+ * @param app
+ * @param req
+ * @param res
+ */
+
 function orderDetailsForRestaurant(app, req, res) {
     restaurantModel.findById(req.session.restaurant._id).then( (restaurant) => {
         restaurant.getOrdersItemsForRestaurant(req.query.orderId)
@@ -101,15 +145,34 @@ function orderDetailsForRestaurant(app, req, res) {
     })
 }
 
+/**
+ * This function renders the list of payment of the restaurant
+ * @param app
+ * @param req
+ * @param res
+ */
 function paymentsList(app, req, res){
     const restaurant = req.session.restaurant;
     res.render('./seller/PaymentsPage.html',{loggedIn : true, name : restaurant.name });
 }
 
+/**
+ * This function renders the seller log in page
+ * @param app
+ * @param req
+ * @param res
+ */
 function loggingIn(app, req, res){
     res.render('./seller/SellerLoginPage.html',
         { loggedIn : req.session.user });
 }
+
+/**
+ * Thsi function renders the restaurant's store with the list of items, groups and catégories
+ * @param app
+ * @param req
+ * @param res
+ */
 
 function sellerStore(app,req, res){
     restaurantModel.findById(req.session.restaurant._id).then(async (restaurant) => {
